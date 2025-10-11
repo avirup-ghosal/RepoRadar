59e568eeba987ba88b282e4b478e152250470b6b
avirup-ghosal <avirupghosal2023@gmail.com>
2025-10-11 19:37:36 +0530

    updated gemini version

--- my-app/src/app/api/gemini/route.ts
+++ my-app/src/app/api/gemini/route.ts
@@ -6,7 +6,9 @@ const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY!);
 export async function POST(req: Request) {
   try {
     const body = await req.json();
-    const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+//     const models = await genAI.listModels();
+// console.log(models);
+    const model = genAI.getGenerativeModel({ model: "gemini-2.5-flash" });
 
     const result = await model.generateContent(body.prompt);
     const response = await result.response;
